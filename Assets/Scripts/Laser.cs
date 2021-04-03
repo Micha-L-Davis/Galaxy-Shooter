946ec08d48b0e93073a7589b33fa3831c9be685c
emeldi <78385236+emeldi@users.noreply.github.com>
2021-04-02 20:00:19 -0700

    Create a space shooter prototype and add assets

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float _speed = 8;
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        if (transform.position.y > 8)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private float _waitTime = 5.0f;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    private bool _stopSpawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator SpawnEnemyRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        while (_stopSpawning == false)
+        {
+            float randomX = Random.Range(-9.5f, 9.5f);
+            GameObject newEnemy = Instantiate(_enemyPrefab, new Vector3(randomX, 8, 0), Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(_waitTime);
+        }
+    }
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+    
+}
