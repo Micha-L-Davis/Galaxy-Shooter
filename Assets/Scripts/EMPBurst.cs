75edd95ec7b61eeccb86cf840df5d542b46b606b
emeldi <78385236+emeldi@users.noreply.github.com>
2021-04-29 11:42:26 -0700

    added explosions to EMP Burst attack

--- Assets/Scripts/EMPBurst.cs
+++ Assets/Scripts/EMPBurst.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class EMPBurst : MonoBehaviour
 {
-
+    [SerializeField]
+    private GameObject _explosionPrefab;
     void Update()
     {
         transform.localScale = Vector3.Lerp(transform.localScale, transform.localScale + new Vector3(1, 1, 0), 10f * Time.deltaTime);
@@ -16,7 +17,12 @@ public class EMPBurst : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Enemy" || other.tag == "Enemy_Laser")
+        if (other.tag == "Enemy")
+        {
+            Instantiate(_explosionPrefab, other.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+        }
+        else if (other.tag == "Enemy_Laser")
         {
             Destroy(other.gameObject);
         }
